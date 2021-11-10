c4f0f1e1d34106bcade0b33865bf2ebb6832f190
girgism <girgis12mirzek@gmail.com>
2021-11-10 23:22:25 +0200

    modifi sserver.js file

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('../dist/angular-directory'))
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/dist/'));
+
+app.all('*', (req, res) => {
+    res.status(200).sendFile(__dirname + '/dist/index.html');
+});
+
+
+app.listen(process.env.PORT || 8080 );
